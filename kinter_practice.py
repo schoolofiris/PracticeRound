40c0a55147effd68c25c2a5c2cd9fdea320b8cd2
schoolofiris <schoolofiris@gmail.com>
2023-04-01 16:17:31 +0400

    a

--- kinter_practice.py
+++ kinter_practice.py
@@ -1,26 +1,59 @@
 from tkinter import *
+from tkinter import messagebox
 from PIL import ImageTk,Image
+def rb_test():
+    value1 = rb1.get()
+    print(value1)
+
+# popup message
+def popup():
+    # showinfo, showwarning, showerror, askquestion, asktocancel, askyesno
+    messagebox.showwarning("This is my pop up","hello world")
+    messagebox.showinfo("This is my pop up","hello world")
+    messagebox.showerror("This is my pop up","hello world")
+    response = messagebox.askokcancel("This is my pop up","hello world")
+    messagebox.askquestion("This is my pop up","hello world")
+    messagebox.askyesno("This is my pop up","hello world")
 
 root = Tk()
 root.title('image test')
 
-my_img = ImageTk.PhotoImage(Image.open("S.png"))
+my_img = ImageTk.PhotoImage(Image.open("D:\CodeDevFolder\PracticeRound\images\S.png"))
 my_label = Label(image = my_img) # one extra step for placing image 
-# image is placed into window as a label
-my_label.pack()
+ # image is placed into window as a label
+my_label.grid(row=0,column=0)
+
+# Adding a status bar and updates dynammically 
+# relief = sunken will give a look of the typical status bar
+# anchor will place the text in left or right like east of west
+status = Label(root, text="image one of five",bd=1,relief=SUNKEN,anchor=E)
+# sticky will span the status bar from east to west
+status.grid(row = 3,column=0,sticky=W+E)
+
+# adding frames
+
+frame = LabelFrame(root, text="this is my frame",padx=150,pady=5,width=20)
+frame.grid(row=2,column=0,padx=25,pady=10)
+
+b2 = Button(frame,text="button 1",pady=5,command=rb_test)
+b2.grid(row=0,column=0)
 
 
 
 
 
+b3 = Button(frame,text="button 2",pady=5,command=popup)
+b3.grid(row=2,column=0)
 
-
-
-
-
+# Radio Button
+rb1 = IntVar()
+Radiobutton(root, text="Option 1", variable=rb1, value=1).grid(row=4,column=0) # when someone clicks on the this option, variable value will be 1
+Radiobutton(root, text="Option 2", variable=rb1, value=2).grid(row=5,column=0)
+rb1.get()
+print(rb1.get())
 
 button_quit = Button(root,text="Exit",command=root.quit)
-button_quit.pack()
+button_quit.grid(row=1,column=0)
 
 
 root.mainloop()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,53 +1,12 @@
-import ipaddress
-import re
+from tkinter import *
+from PIL import ImageTk,Image
 
-app_loop = ''
-while(app_loop!= 'n'):
-    # input_IP = '192.168.0.129'
-    # mask1 = '24'
-    # mask2 = '255.255.255.0'
-    input_IP = input("Enter the IP address \t: ")
-    if re.fullmatch(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}",input_IP):
-        octet_list = input_IP.split('.')
-        if (
-            len(octet_list) == 4 
-            and 1 <= int(octet_list[0]) <= 223 
-            and int(octet_list[0]) != 127 
-            and (int(octet_list[0]) != 169 or int(octet_list[1]) != 254)
-            and 0 <= int(octet_list[1]) <= 255 
-            and 0 <= int(octet_list[2]) <= 255 
-            and 0 <= int(octet_list[3]) <= 255
-            ):
-            mask_input = input("Enter subnet mask \t: ")
-            if re.fullmatch(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}",mask_input):   
-                # following code will strip the subnet mask 
-                # and calculate the number of subnets and number of hosts
-                str_masksplit = mask_input.split('.')
-                int_Masksplit = [int(mask) for mask in str_masksplit]
-                bin_masksplit = [(bin(m).lstrip('0b')).zfill(8) for m in int_Masksplit]
-                bin_mask = ''.join(bin_masksplit)
-                number_host = 2**(bin_mask.count('0'))-2
-                number_subnet = 2**(bin_mask.count('1'))
-                print(f"Number of subnets \t: {number_subnet} \nNumber of hosts \t: {number_host}")
-                hostIP = input_IP + '/' + str(bin_mask.count('1'))
-            elif re.fullmatch(r"[0-9]{1,2}",mask_input) and 0<=int(mask_input)<=32:
-                hostIP = input_IP + '/' + mask_input
-                number_host = 2**(abs(32-int(mask_input)))-2
-                number_subnet = 2**(int(mask_input))
-                print(f"Number of subnets \t: {number_subnet} \nNumber of hosts \t: {number_host}")
-            else:
-                print("INVALID SUBNET MASK!!! TRY AGAIN")
-                continue
-            # following code will find the network address from the given interface address and mask
-            print(f"Interface IP \t\t: {input_IP}")
-            print("Network Address \t: "+str((ipaddress.ip_interface(hostIP)).network))
-            print("Starting Address \t: "+str(ipaddress.ip_interface(hostIP)+1))
-            print("Ending Address \t\t: "+str(ipaddress.ip_interface(hostIP)+number_host))
-        else:
-            app_loop = input("INVALID IP. TRY AGAIN? (y/n)\t:")
-            
-    else:
-        exit()
-        app_loop = input("INVALID IP. TRY AGAIN? (y/n)\t:")
-        
-    app_loop = input("\nEnter new address?(y/n)\t: ")
+root = Tk()
+root.title('image test')
+
+# my_img = ImageTk.PhotoImage(Image.open("S.png"))
+# my_label = Label(image = my_img) # one extra step for placing image 
+# # image is placed into window as a label
+# my_label.pack()
+
+# Adding a status bar and updates dynammically 
