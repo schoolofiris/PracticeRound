fb6c8fef645565871183b60d46a349b513c80b71
schoolofiris <schoolofiris@gmail.com>
2023-04-03 16:00:47 +0400

    a

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,4 @@
-    # loop through results 
-    for record in records:
-        f_name_editor.insert(0,record[0])
-        l_name_editor.insert(0,record[1])
-        address_editor.insert(0,record[2])
-        city_editor.insert(0,record[3])
-        state_editor.insert(0,record[4])
-        zipcode_editor.insert(0,record[5])
+mylabel = Label(root, text=api[0]['AQI'])
+# mylabel.pack()
+# mylabel = Label(root, text=api[0]['Category']['Name'])
+# mylabel.pack()
